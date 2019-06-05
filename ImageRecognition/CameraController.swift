3b04c2056b4faf9cc5399de4335dc866e592b449
Makan Fofana <mfofanaT99@gmail.com>
2019-06-05 10:47:12 -0400

    Initial Commit

---
+++ ImageRecognition/CameraController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Makan Fofana
+
+import UIKit
+import AVFoundation
+
+class CameraController: UIViewController {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var captureSession = AVCaptureSession()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        // Start video capture.
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        captureSession.stopRunning()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    private func configure() {
+        // Get the back-facing camera for capturing videos
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            print("Failed to get the camera device")
+            return
+        }
+ 
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session.
+            captureSession.addInput(input)
+            let videoDataOutput = AVCaptureVideoDataOutput()
+            videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "imageRecognition.queue"))
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            captureSession.addOutput(videoDataOutput)
+            
+            
+            
+        } catch {
+            // If any error occurs, simply print it out and don't continue any more.
+            print(error)
+            return
+        }
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Bring the label to the front
+        descriptionLabel.text = "Looking for objects..."
+        view.bringSubview(toFront: descriptionLabel)
+    }
+}
+
+
+
+
+
+
+
+extension CameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        connection.videoOrientation = .portrait
+        
+        //Resizing the frame to 299*299
+        //The required size of the inceptionV3 model
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+
+
